f52da2a38c8126aa6331fd8a5940995bff1fd140
ChoiJaeEun <poungki0823@gmail.com>
2022-03-27 00:58:52 +0900

    redis 설정

---
+++ apigateway/src/main/java/com/sesac/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.sesac.config;
+
+import com.sesac.jwt.Token;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> tokenRedisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        // Token 객체를 json 형태로 깨지지 않고 받기 위한 직렬화
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Token.class));
+        return redisTemplate;
+    }
+}
