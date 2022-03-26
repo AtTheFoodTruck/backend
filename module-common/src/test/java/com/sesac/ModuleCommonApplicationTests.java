4e3cf099a6949e9c4e8cb498dffe6097e5dba056
jaeminLee524 <rkwoals524@hanmail.net>
2022-03-26 17:08:55 +0900

    BaseEntity 생성 및 테스트완료(임시 uuid로 진행)

--- module-common/src/test/java/com/sesac/ModuleCommonApplicationTests.java
+++ module-common/src/test/java/com/sesac/ModuleCommonApplicationTests.java
@@ -1,9 +1,24 @@
 package com.sesac;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class ModuleCommonApplicationTests {
+
+    @Bean
+    public AuditorAware<String> auditor() {
+
+        // TODO security context에 저장된 user 정보를 return해줘야 함
+        // 우선 랜덤 ID
+        return () -> Optional.of(UUID.randomUUID().toString());
+    }
 
     public void contextLoads() {
 
---
+++ module-common/src/test/java/com/sesac/domain/member/entity/MemberTest.java
@@ -0,0 +1,54 @@
+package com.sesac.domain.member.entity;
+
+import com.sesac.domain.member.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@Transactional
+@Rollback(value = false)
+@SpringBootTest
+public class MemberTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+    @PersistenceContext
+    private EntityManager em;
+
+    @Test
+    public void BaseEntity_테스트() throws InterruptedException {
+        //given
+        Member member = new Member();
+        member.setName("jaemin");
+        member.setEmail("jaemin@naver.com");
+        memberRepository.save(member);
+
+        Thread.sleep(100);
+        member.setName("jaeminUpdate");
+        member.setEmail("jaeminUpdate@naver.com"); //modify
+
+        em.flush();
+        em.clear();
+
+        //when
+        Member findMember = memberRepository.findById(member.getId()).orElseThrow(
+                () -> new IllegalArgumentException("id에 해당하는 멤버 없음. id = " + member.getId())
+        );
+
+        //then
+        System.out.println("createdDate = " + findMember.getCreatedDate());
+        System.out.println("createdBy = " + findMember.getCreatedBy());
+        System.out.println("modifiedDate = " + findMember.getModifiedDate());
+        System.out.println("modifiedBy = " + findMember.getModifiedBy());
+    }
+}
