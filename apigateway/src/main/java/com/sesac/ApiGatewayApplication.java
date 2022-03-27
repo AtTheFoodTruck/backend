429f5a72c265211a422c66985633f21855a41a62
ChoiJaeEun <poungki0823@gmail.com>
2022-03-27 15:46:18 +0900

    불필요한 import 제거

--- apigateway/src/main/java/com/sesac/ApiGatewayApplication.java
+++ apigateway/src/main/java/com/sesac/ApiGatewayApplication.java
@@ -1,13 +1,8 @@
 package com.sesac;
 
-import com.sesac.jwt.JwtAuthenticationGatewayFilterFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @EnableEurekaClient
