069452ab638bba4a2742b42ac71d5c4830bd01fc
ChoiJaeEun <poungki0823@gmail.com>
2022-03-27 22:57:22 +0900

    불필요한 method 제거 및 리팩토링

--- apigateway/src/main/java/com/sesac/jwt/JwtTokenProvider.java
+++ apigateway/src/main/java/com/sesac/jwt/JwtTokenProvider.java
@@ -3,29 +3,28 @@ package com.sesac.jwt;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
+import java.security.Key;
 import java.util.*;
-import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtTokenProvider implements Serializable {
     private static final long serialVersionUID = -2550185165626007488L;
-    private static final String REFRESH_TOKEN_KEY = "RF";
+
+    private static final String AUTHORITIES_KEY = "auth";
+
     private static final long JWT_ACCESS_TOKEN_VALIDITY = 60 * 30; // 30 minutes
     private static final long JWT_REFRESH_TOKEN_VALIDITY = 60 * 60 * 24 * 7; // 1 week
 
-    @Value("${jwt.secret}")
-    private String secret;
-    @Autowired
-    private RedisTemplate redisTemplate;
+    private Key key;
 
     /**
      * token 의 모든 claim 반환
@@ -35,31 +34,11 @@ public class JwtTokenProvider implements Serializable {
      * 작성일 2022/03/27
      **/
     private Claims getAllClaimsFromToken(String token) {
-        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
-    }
-
-    /**
-     * token 에서 원하는 claim 반환
-     * @param token: jwt token
-     * @param claimsResolver: token 에서 얻고자하는 claim method
-     * @author jjaen
-     * @version 1.0.0
-     * 작성일 2022/03/27
-    **/
-    private <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = getAllClaimsFromToken(token);
-        return claimsResolver.apply(claims);
-    }
-
-    /**
-     * token 에서 username 반환
-     * @param accessToken: jwt access token
-     * @author jjaen
-     * @version 1.0.0
-     * 작성일 2022/03/27
-    **/
-    public String getUsernameFromToken(String accessToken) {
-        return getClaimFromToken(accessToken, Claims::getSubject);
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 
     /**
@@ -70,70 +49,65 @@ public class JwtTokenProvider implements Serializable {
      * 작성일 2022/03/27
     **/
     public Map<String, Object> getUserParseInfo(String accessToken) {
-        Claims parseInfo = getAllClaimsFromToken(accessToken);
+        Claims claims = getAllClaimsFromToken(accessToken);
         Map<String, Object> result = new HashMap<>();
-        result.put("username", parseInfo.getSubject());
-        result.put("role", parseInfo.get("role", List.class));
+        result.put("email", claims.getSubject());
+        result.put(AUTHORITIES_KEY, claims.get(AUTHORITIES_KEY));
 
         return result;
     }
 
     /**
-     * token 만료 여부
-     * @param token: jwt token
-     * @author jjaen
-     * @version 1.0.0
-     * 작성일 2022/03/27
-     **/
-    public Boolean isTokenExpired(String token) {
-        return getClaimFromToken(token, Claims::getExpiration).before(new Date());
-    }
-
-    /**
      * Access Token 생성
-     * @param userDetails
+     * @param authentication
      * @author jjaen
      * @version 1.0.0
      * 작성일 2022/03/27
     **/
-    public String generateAccessToken(UserDetails userDetails) {
-        Map<String, Object> claims = new HashMap<>();
-        List<String> authorities = new ArrayList<>();
+    public String generateAccessToken(Authentication authentication) {
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
 
-        for (GrantedAuthority a: userDetails.getAuthorities()) {
-            authorities.add(a.getAuthority());
-        }
-
-        claims.put("role", authorities);
-        return Jwts.builder().setClaims(claims)
-                .setSubject(userDetails.getUsername())
+        return Jwts.builder()
+                .claim(AUTHORITIES_KEY, authorities)  // authorities
+                .setSubject(authentication.getName())  // email
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + JWT_ACCESS_TOKEN_VALIDITY * 1000))
-                .signWith(SignatureAlgorithm.HS512, secret).compact();
+                .signWith(key, SignatureAlgorithm.HS512).compact();
     }
 
     /**
      * Refresh Token 생성
-     * @param username
+     * @param authentication
      * @author jjaen
      * @version 1.0.0
      * 작성일 2022/03/27
      **/
-    public String generateRefreshToken(String username) {
-        return Jwts.builder().setSubject(username)
+    public String generateRefreshToken(Authentication authentication) {
+        return Jwts.builder()
+                .setSubject(authentication.getName())  // email
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + JWT_REFRESH_TOKEN_VALIDITY * 1000))
-                .signWith(SignatureAlgorithm.HS512, secret).compact();
+                .signWith(key, SignatureAlgorithm.HS512).compact();
     }
+
     /**
-     * Redis 에 Refresh Token 저장
-     * @param token: username(key), refreshToken(value)
+     * access token 에 담겨있는 권한 정보들(claims)을 이용해 Authentication 객체 리턴
      * @author jjaen
      * @version 1.0.0
      * 작성일 2022/03/27
-     **/
-    private void saveRefreshToken(Token token) {
-        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
-        valueOperations.set(token.getUsername(), token.getRefreshToken());
+    **/
+    public Authentication getAuthentication(String token) {
+        Claims claims = getAllClaimsFromToken(token);
+
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        User principal = new User(claims.getSubject(), "", authorities);
+
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
     }
 }
